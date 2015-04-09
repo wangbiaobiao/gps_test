cdfb01fdcbd4864b582284593d522dbefabfdbf4
wangbiaobiao <wang_shengbiao@qq.com>
2015-04-09 00:24:16 -0700

    clean

--- qt-led.sh
+++
@@ -1,2 +0,0 @@
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/bin:/usr/sbin:/sbin:/bin:/usr/games:/usr/local/games:/home/wangsb/9260_sdk/host/usr/bin:/home/wangsb/platform/gcc-linaro-arm-linux-gnueabihf-4.7-2012.12-20121214_linux/bin
-export QMAKESPEC=/home/wangsb/9260_sdk/host/usr/arm-biaobiao-linux-gnueabi/sysroot/usr/mkspecs/default
